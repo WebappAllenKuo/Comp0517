8c7ae1fd84b4c1fbe1c454fce7201924c9341e5f
allen <g01.webapp@gmail.com>
2024-05-17 21:17:54 +0800

    add sh file

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import ViewA from './views/ViewA.vue'
+import ViewB from './views/ViewB.vue'
+import { RouterView, RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div>
+    <router-link to="/">View A</router-link> | 
+    <router-link to="/view-b">View B</router-link>
+  </div>
+  <router-view />
+    
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/ViewB.vue
@@ -0,0 +1,11 @@
+<script setup >
+
+</script>
+
+<template>
+    <h1>View B</h1>
+</template>
+
+<style scoped>
+
+</style>
