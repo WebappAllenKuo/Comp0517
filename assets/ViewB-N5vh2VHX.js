e65ba0b886f6f4bd513c06d33de19194bf8885ea
allen <g01.webapp@gmail.com>
2024-05-17 21:26:14 +0800

    deploy

--- assets/ViewB-N5vh2VHX.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o as c,c as o}from"./index-aND0DP4L.js";const r={};function t(n,s){return c(),o("h1",null,"View B")}const f=e(r,[["render",t]]);export{f as default};
