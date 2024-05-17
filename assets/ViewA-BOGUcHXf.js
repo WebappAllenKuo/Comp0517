40d202f1c3ee9bbc3cb1a99adf32e4d8d724c338
allen <g01.webapp@gmail.com>
2024-05-17 21:50:36 +0800

    deploy

--- assets/ViewA-BOGUcHXf.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o as c,c as o}from"./index-Cl3Mr6mu.js";const r={};function t(n,s){return c(),o("h1",null,"View A")}const f=e(r,[["render",t]]);export{f as default};
